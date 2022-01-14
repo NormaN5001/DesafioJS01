9e00541eca75c605ab095e4fd8c56ce23b3dc732
Nícolas Goulart <nicolas.goulartsouza123@gmail.com>
2022-01-14 11:44:24 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "VaiNaWeb"
+let number = 43
+let anoAtual = 2022 - number
+console.log(anoAtual + number)
+console.log("Estou mexendo no console.log")
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
